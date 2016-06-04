cfeb6dcc1bcea7416956a5a8a4389c12e5f082b0
jai-angle <Jai_Angle@hotmail.com>
2016-06-04 13:05:09 +1000

    Update DownloadGandCData.R

--- DownloadGandCData.R
+++ DownloadGandCData.R
@@ -3,5 +3,5 @@
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile="Datafile.zip")
 
-###Unzip DataSet to /data directory
+###Unzip DataSet to the current directory
 unzip("DataFile.zip", exdir =".", overwrite=TRUE)
