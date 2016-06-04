5eb30dd1b91c585112a9bf32b18b17dc8b068112
jai-angle <Jai_Angle@hotmail.com>
2016-06-04 13:04:11 +1000

    Add files via upload

---
+++ DownloadGandCData.R
@@ -0,0 +1,7 @@
+#This code downloads the file as required for Getting and Cleaning Data project.
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile="Datafile.zip")
+
+###Unzip DataSet to /data directory
+unzip("DataFile.zip", exdir =".", overwrite=TRUE)
